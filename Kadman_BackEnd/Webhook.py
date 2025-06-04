c5ea9809a4fded631597b84dbb87da0a90de41fa
pattaranon_detnanta <pattaranon_detnanta@cmu.ac.th>
2025-06-04 17:23:22 +0700

    Change UserId to primary key and check for duplicated_vendors

--- Kadman_BackEnd/Webhook.py
+++ Kadman_BackEnd/Webhook.py
@@ -3,6 +3,8 @@ from dotenv import load_dotenv
 import os
 import requests
 from db import wait_for_db_connection
+import mysql.connector
+
 app = Flask(__name__)
 load_dotenv()
 CHANNEL_ACCESS_TOKEN = os.getenv('LineOA_KEY') 
@@ -17,6 +19,7 @@ def webhook():
             user_profile = get_user_profile(user_id)
             print("Webhook received!")
             print("New User Followed:", user_id)
+            print("User Profile:", user_profile)
             insert_waiting_vendor(user_id, user_profile)
     return 'OK', 200
 
@@ -36,11 +39,19 @@ def insert_waiting_vendor(user_id, user_profile):
     cursor = conn.cursor()
     display_name = user_profile.get('displayName', '') if user_profile else ''
     sql = "INSERT INTO waiting_vendors (LineID, UserProfile) VALUES (%s, %s)"
-    cursor.execute(sql, (user_id, display_name))
-    conn.commit()
-    cursor.close()
-    print(f"Inserted waiting vendor: {user_id}, DisplayName: {display_name}")
-
+    
+    try:
+        cursor.execute(sql, (user_id, display_name))
+        conn.commit()
+        print(f"Inserted waiting vendor: {user_id}, DisplayName: {display_name}")
+    except mysql.connector.errors.IntegrityError as err:
+        if err.errno == 1062:  # Duplicate entry
+            print(f"User {user_id} is already in waiting_vendors. No need to insert again.")
+        else:
+            print(f"Database error: {err}")
+    finally:
+        cursor.close()
+        conn.close()
 
 if __name__ == '__main__':
     app.run(port=5000)
